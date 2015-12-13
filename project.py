8c19d383277740b4693192484b27bfa31de54c13
RPS5555 <chachapol13@hotmail.com>
2015-12-13 23:36:01 +0700

    Start
    print "THE SITUATION OF TRAFFIC ACCIDENT CAUSE OF THE ACCIDENT BY A
    PERSON AND ENVIRONMENT \
    CAUSES OF THE EQUIPMENT USED IN DRIVING ,WHOLE KINGDOM: 2006 - 2013" and
    take input

---
+++ project.py
@@ -0,0 +1,7 @@
+def interface():
+    '''this function user must take your _input what do you want to know about grahp of accident'''
+print("THE SITUATION OF TRAFFIC ACCIDENT CAUSE OF THE ACCIDENT BY A PERSON AND ENVIRONMENT \
+CAUSES OF THE EQUIPMENT USED IN DRIVING ,WHOLE KINGDOM: 2006 - 2013")
+print('which graph do you want to see'+'('+'all/conclude/each situation'+')') #ให้ผู้ใช้เลือกกราฟที่ต้องการให้แสดง
+ans = input()
+interface()
